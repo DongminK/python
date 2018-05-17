3fdd79692dd8e666dea0ba43ee221a44be0fe601
DongminK <kdm0228@gmail.com>
2018-05-17 10:44:26 +0900

    예제추가

--- arr_calculator.py
+++ arr_calculator.py
@@ -20,10 +20,18 @@ class Calculator:
 		return total / size
 
 
-cal1 = Calculator([1,2,3,4,5])
-print(cal1.sum())  # 15 출력
-print(cal1.avg())  # 3.0 출력
+if __name__ == '__main__':
 
-cal2 = Calculator([6,7,8,9,10])
-print(cal2.sum())  # 40 출력
-print(cal2.avg())  # 8.0 출력
+	cal1 = Calculator([1,2,3,4,5])
+	print(cal1.sum())  # 15 출력
+	print(cal1.avg())  # 3.0 출력
+
+	cal2 = Calculator([6,7,8,9,10])
+	print(cal2.sum())  # 40 출력
+	print(cal2.avg())  # 8.0 출력
+
+	try:
+		b = 10 / 0
+	except:
+		print("Error")
+
